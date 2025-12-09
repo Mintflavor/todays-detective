b9dd191a616b379a59413eaebdceaf56347d158c
mintflavor <hamer1009@gmail.com>
2025-12-09 22:29:13 +0900

    .

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -12,7 +12,7 @@ export async function POST(req) {
 
     // 2. Google Gemini API 호출
     // const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`, {
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemma-3-27b-it:generateContent?key=${apiKey}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${apiKey}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ contents: [{ parts: [{ text: prompt }] }] })
